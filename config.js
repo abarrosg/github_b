bf0fb5767b81d71f006b6ffd1063e7e50a02358b
Anthony barros <abarrosg@est.ups.edu.ec>
2020-10-02 01:43:59 -0500

    Termino final semianrio issacdb

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.nfhiu.gcp.mongodb.net/utups?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.nfhiu.gcp.mongodb.net/upssalesiana?retryWrites=true&w=majority',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE|| '/',
